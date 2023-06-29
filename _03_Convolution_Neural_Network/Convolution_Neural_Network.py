42d5d6cd84dfab1fe234bcd55c1c93365a71c02e
= <=>
2023-06-29 15:37:17 +0800

    6

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -21,9 +21,6 @@ transform = transforms.Compose([
     transforms.RandomCrop(32),
     transforms.ToTensor()])
 
-
-
-
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3,
                      stride=stride, padding=1, bias=False)
